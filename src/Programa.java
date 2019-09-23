7c53ed92055f128c09ed746b81335aad220dcfb0
Leonardo-Fernandes-est <leo35.shalom@gmail.com>
2019-09-23 17:29:38 -0300

    multiplicacao feita

--- src/Programa.java
+++ src/Programa.java
@@ -1,9 +1,18 @@
 
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Operação de multiplicação");
+		System.out.println("Digite o valor 1");
+		Scanner teclado = new Scanner(System.in);
+		int valor1 = teclado.nextInt();
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.println("Digite o valor 2");
+		int valor2 = teclado.nextInt();
+		int resultado = valor1 * valor2;
+		System.out.println("A multiplicação dos dois valores é " + resultado);
 	}
-
 }
