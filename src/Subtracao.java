55052212fe7e66aad5766c778dcaf82343d91857
Leonardo-Fernandes-est <leo35.shalom@gmail.com>
2019-09-25 14:28:30 -0300

    totalmente atualizado e eh ruim de aturar, bomba patch virou moda todo mundo quer jogar

--- src/Subtracao.java
+++ src/Subtracao.java
@@ -1,18 +1,17 @@
-
 import java.util.Scanner;
 
-public class Programa {
-//MULTIPLICACAO
+public class Subtracao {
+
 	public static void main(String[] args) {
 
-		System.out.println("Operação de multiplicação");
+		System.out.println("Operação de Subtração");
 		System.out.println("Digite o valor 1");
 		Scanner teclado = new Scanner(System.in);
 		double valor1 = teclado.nextDouble();
 		System.out.println("-----------------------------------------------------------------------");
 		System.out.println("Digite o valor 2");
 		double valor2 = teclado.nextDouble();
-		double resultado = valor1 * valor2;
+		double resultado = valor1 - valor2;
 		System.out.println("A multiplicação dos dois valores é " + resultado);
 	}
 }
