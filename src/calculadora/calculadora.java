bb993576bf9e444882367501c5e9ae8b6e556c80
Leonardo-Fernandes-est <leo35.shalom@gmail.com>
2019-09-25 16:09:23 -0300

    aaaa

--- src/calculadora/calculadora.java
+++ src/calculadora/calculadora.java
@@ -3,45 +3,75 @@ package calculadora;
 import java.util.Scanner;
 
 public class calculadora {
+	boolean tentarDnv;
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("(FLEX-CALC) Escolha a operação desejada:     ");
 		System.out.println();
 		System.out.println("1- Soma          2 - Divisão");
 		System.out.println("3 - Subtração    4 - Multiplicação");
-		int operador;
-		operador = entrada.nextInt();
-		//SOMAR
-		if(operador == 1) { 
-		System.out.println("Digite o primeiro numero: ");
-		float numero1 = entrada.nextInt();
 		
-		System.out.println("Digite o segundo numero: ");
-		float numero2 = entrada.nextInt();
-		
-		float resultado = numero1 + numero2;
-		
-		System.out.println("A soma é igual a: " + resultado);
-		//DIVISÃO
-		}if(operador == 2) {
-			System.out.println("Digite o primeiro numero: ");
-			float numero1 = entrada.nextInt();
-			System.out.println("Digite o segundo numero: ");
-			float numero2 = entrada.nextInt();
+		do{
+			int operador;
+			operador = entrada.nextInt();
+			//SOMAR
+			if(operador == 1) { 
+				System.out.println("Digite o primeiro numero: ");
+				double numero1 = entrada.nextDouble();
+
+				System.out.println("Digite o segundo numero: ");
+				double numero2 = entrada.nextDouble();
+
+				double resultado = numero1 + numero2;
+
+				System.out.println("A soma é igual a: " + resultado);
+
+			//DIVISÃO
+			}if(operador == 2) {
+				System.out.println("Digite o primeiro numero: ");
+				double numero1 = entrada.nextInt();
+				System.out.println("Digite o segundo numero: ");
+				double numero2 = entrada.nextInt();
+				double resultado = numero1 / numero2;
+
+				System.out.println("A Divisão é igual a: " + resultado);
+
+				if(numero2 == 0) {
+					System.out.println(" A divisão por zero não é definida ");
+
+			//SUBTRAÇÃO
+			}}if(operador == 3) {
+				System.out.println("Digite o primeiro numero: ");
+				double numero1 = entrada.nextDouble();
+				System.out.println("Digite o segundo numero: ");
+				double numero2 = entrada.nextDouble();
+				double resultado = numero1 - numero2;
+
+					System.out.println("A multiplicação é igual a: " + resultado);
+			//MULTIPLICAÇÃO
+			}if(operador == 4) {
+				System.out.println("Digite o primeiro numero: ");
+				double numero1 = entrada.nextDouble();
+				System.out.println("Digite o segundo numero: ");
+				double numero2 = entrada.nextDouble();
+				double resultado = numero1 * numero2;
+					System.out.println("A multiplicação é igual a: " + resultado);
+				}if(operador > 4){
+					System.out.println("Digitou errado, tente novamente");
+				}
+				System.out.println("Você deseja continuar? Digite true para sim e false para não");
+				Scanner booleano = new Scanner(System.in);
+				boolean tentarDnv = booleano.hasNextBoolean();
+		}while (tentarDnv == true);
 			
-			float resultado = numero1 / numero2;
-			
-			System.out.println("A Divisão é igual a: " + resultado);
-			
-			if(numero2 == 0) {
-				System.out.println(" A divisão por zero não é definida ");
-			}
 		}
 		}
-		}
-		
-		
+	
+
+
+
+
 					
 			
 				
